a0d5053fc517dc2123b37a79dd3affda9e55535e
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2023-11-14 20:58:21 +0000

    New login page

---
+++ src/resources/views/header.blade.php
@@ -0,0 +1,36 @@
+<h3>Header Page</h3>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home </a>
+      </li>
+      <li class="">
+        <a class="nav-link" href="#">Orders </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Cart(0)</a>
+      </li>
+    </ul>
+    <nav class="navbar navbar-light bg-light justify-content-between">
+    <form >
+        <div class="form-group">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </div>
+    </form>
+
+</nav>
+  </div>
+</nav>
