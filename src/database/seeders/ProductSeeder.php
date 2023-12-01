f057d354290e51c81087fa43a52779de1df9e36c
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2023-12-01 12:38:02 +0000

    Pupulate product table

--- src/database/seeders/ProductSeeder.php
+++ src/database/seeders/ProductSeeder.php
@@ -13,7 +13,7 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //populate table products
         DB::table('products')->insert([
             [
                 'name'=> 'Oppo mobile',
