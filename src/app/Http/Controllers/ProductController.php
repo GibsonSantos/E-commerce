c9a9f39b2d17fb6dc546dae93af38eb4ef13f8a0
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2023-12-05 19:58:19 +0000

    Update Product detail

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -7,7 +7,7 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    
     function index(){
         $data = Product::all();
         return view('product', ['products'=>$data]);
