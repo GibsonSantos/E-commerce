423212627bfeb08d7f4a84352d49d6b1229413a0
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2023-12-13 12:41:52 +0000

    Update header.blade.php

--- src/resources/views/header.blade.php
+++ src/resources/views/header.blade.php
@@ -1,13 +1,15 @@
 <?php
 use App\Http\Controllers\ProductController;
-$total = ProductController::cartItem();
-
+$total=0;
+if(Session::has('user')){
+  $total = ProductController::cartItem();
+}
 
 ?>
 
 <h3>Header Page</h3>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/">E-Comm</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -15,7 +17,7 @@ $total = ProductController::cartItem();
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home </a>
+        <a class="nav-link" href="\">Home </a>
       </li>
       <li class="">
         <a class="nav-link" href="#">Orders </a>
@@ -26,6 +28,24 @@ $total = ProductController::cartItem();
 
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Cart({{$total}})</a>
+        @if(Session::has('user'))
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {{Session::get('user')['name']}}
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/logout">Logout</a>
+      </li>
+      @else
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Menu
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/login">Login</a>
+      </li>
+      @endif
+
       </li>
     </ul>
     <nav class="navbar navbar-light bg-light justify-content-between">
