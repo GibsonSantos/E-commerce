3fe4ed7a112386edf1d355f04cf2ba99423f264d
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2023-12-13 15:04:08 +0000

    Update ProductController.php

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -34,5 +35,15 @@ class ProductController extends Controller
     static function cartItem(){
         $userID = Session::get('user')['id'];
         return Cart::where('user_id', $userID)->count();
+    }
+
+    function cardList(){
+        $userID = Session::get('user')['id'];
+        $products = DB::table('cart')
+        ->join('products', 'cart.product_id', '=','products.id')
+        ->where('cart.user_id', $userID)
+        ->select('products.*')
+        ->get();
+        return view('cartlist',['products'=>$products]);
     }
 }
