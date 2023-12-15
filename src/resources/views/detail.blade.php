4feedbfaa6e5408ef34085f448e07a3b83a9e854
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2023-12-15 15:32:15 +0000

    Update detail.blade.php

--- src/resources/views/detail.blade.php
+++ src/resources/views/detail.blade.php
@@ -12,11 +12,17 @@
             <h4>Details : {{$product['description']}}</h4>
             <h4>Details : {{$product['category']}}</h4>
             <br><br>
+            @if(Session::has('user'))
             <form action="/add_to_cart" method="POST">
                 @csrf
                 <input type="text" name="product_id" value="{{$product['id']}}" hidden>
                 <button class="btn btn-primary" >Add to CART</button>
             </form>
+            @else
+            <form action="/login" method="GET">
+                <button class="btn btn-primary" >Add to CART</button>
+            </form>
+            @endif
             <br><br>
             <button class="btn btn-success" >Buy Now</button>
         </div>
