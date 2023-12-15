f20beac1b1ad4ca5ae2bfa0aff9e6d0d84d37ab9
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2023-12-15 15:27:57 +0000

    Update ProductController.php

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product;
+use App\Models\Product; 
 use App\Models\Cart;
+use App\Models\Order;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
 
@@ -42,8 +43,57 @@ class ProductController extends Controller
         $products = DB::table('cart')
         ->join('products', 'cart.product_id', '=','products.id')
         ->where('cart.user_id', $userID)
-        ->select('products.*')
+        ->select('products.*', 'cart.id as cart_id')
         ->get();
         return view('cartlist',['products'=>$products]);
     }
+
+    function removeCart($id){
+        Cart::destroy($id);
+        return redirect('/cartlist');
+    }
+
+
+    function orderNow(){
+        $userID = Session::get('user')['id'];
+        $total = DB::table('cart')
+        ->join('products', 'cart.product_id', '=','products.id')
+        ->where('cart.user_id', $userID)
+        ->sum('products.price');
+
+
+        return view('ordernow', ['total'=>$total]);
+    }
+
+    function orderplace(Request $req){
+        $userID = Session::get('user')['id'];
+        $allCart = Cart::where('user_id', $userID)->get();
+
+        foreach($allCart as $cart){
+            $order = new Order();
+            $order->product_id=$cart['product_id'];
+            $order->user_id=$userID;
+            $order->status="pending";
+            $order->payment_method=$req->payment;
+            $order->payment_status="pending";
+            $order->address=$req->address;
+            $order->save();
+            Cart::where('user_id', $userID)->delete();
+        }
+
+        return redirect('/');
+    }
+
+    function myOrders(){
+        $userID = Session::get('user')['id'];
+        $orders = DB::table('orders')
+        ->join('products', 'orders.product_id', '=','products.id')
+        ->where('orders.user_id', $userID)
+        ->get();
+
+
+        return view('myorders', ['orders'=>$orders]);
+    }
 }
+
+
