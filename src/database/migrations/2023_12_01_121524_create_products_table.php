fbe2c14a242d400bcb29bb1eed79f17c94abdc56
Gibson Oliveira dos Santos Filho <98161396+GibsonSantos@users.noreply.github.com>
2024-03-12 09:18:02 +0000

    Update 2023_12_01_121524_create_products_table.php

--- src/database/migrations/2023_12_01_121524_create_products_table.php
+++ src/database/migrations/2023_12_01_121524_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("name");
             $table->decimal("price");
-            $table->integer("category");
+            $table->string("category");
             $table->string("description");
             $table->string("gallery");
             $table->timestamps();
